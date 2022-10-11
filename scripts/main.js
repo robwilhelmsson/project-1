4120e88a4097f528707ffa60c2496225f631143f
Rob Wilhelmsson <rob.wilhelmsson@gmail.com>
2022-10-11 22:04:06 +0100

    movement almost complete

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,10 @@
+// ! DOM Selectors
 const canvas = document.querySelector('.canvas')
 const ctx = canvas.getContext('2d')
 
-const gravity = 0.5
+
+// ! Variables
+const gravity = 0.18
 const keys = {
   right: {
     pressed: false,
@@ -9,22 +12,21 @@ const keys = {
   left: {
     pressed: false,
   },
-  up: {
-    pressed: false,
-  },
 }
+let lastKey
+
 
 // ! Creating the class for player
 class Player {
   constructor() {
     // * Player start position
     this.position = {
-      x: 200,
-      y: 450,
+      x: 300,
+      y: 250,
     }
     // * Player size & velocity
-    this.width = 10
-    this.height = 20
+    this.width = 8
+    this.height = 14
     this.velocity = {
       x: 0,
       y: 0,
@@ -49,15 +51,16 @@ class Player {
   }
 }
 
+
 // ! Creating class for the platforms
 class Platform {
-  constructor() {
+  constructor(x, y) {
     this.position = {
-      x: 250,
-      y: 420,
+      x,
+      y,
     }
-    this.width = 200
-    this.height = 20
+    this.width = 14
+    this.height = 60
   }
   draw() {
     ctx.fillStyle = 'black'
@@ -65,65 +68,71 @@ class Platform {
   }
 }
 
-// * Create new player & platform
+
+// ! Create new player & platform
 const player = new Player()
-const platform = new Platform()
+const platforms = [ new Platform(250, 420)
+]
 
 
-// * Passing the update function to animate
+// ! Create new player & platform
 function animate() {
   requestAnimationFrame(animate)
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   player.update()
-  platform.draw()
+  platforms.draw()
   // * Moving direction for player 
-  if (keys.right.pressed) {
-    player.velocity.x = 5
-  } else if (keys.left.pressed) {
-    player.velocity.x = -5
+  player.velocity.x = 0
+  if (keys.right.pressed && lastKey === 39) {
+    player.velocity.x = 3
+  } else if (keys.left.pressed && lastKey === 37) {
+    player.velocity.x = -3
   } else {
     player.velocity.x = 0
   }
+
   // * Collision detection top of platform
-  if (player.position.y + player.height <= platform.position.y
-    && player.position.y + player.height + player.velocity.y >= platform.position.y
-    && player.position.x + player.width >= platform.position.x
-    && player.position.x <= platform.position.x + platform.width) {
+  if (player.position.y + player.height <= platforms.position.y
+    && player.position.y + player.height + player.velocity.y >= platforms.position.y
+    && player.position.x + player.width >= platforms.position.x
+    && player.position.x <= platforms.position.x + platforms.width) {
     player.velocity.y = 0
   }
   // * Collision detection bottom of platform
-  if (player.position.y >= platform.position.y - platform.height
-    && player.position.y <= platform.position.y + platform.height
-    && player.position.x + player.width >= platform.position.x
-    && player.position.x <= platform.position.x + platform.width) {
+  if (player.position.y >= platforms.position.y - platforms.height
+    && player.position.y <= platforms.position.y + platforms.height
+    && player.position.x + player.width >= platforms.position.x
+    && player.position.x <= platforms.position.x + platforms.width) {
     player.velocity.y = 1
   }
   // * Collision detection sides of platform
-  if (player.position.x + player.width >= platform.position.x 
-    && player.position.x <= platform.position.x + platform.width
-    && player.position.y + player.height >= platform.position.y
-    && player.position.y <= platform.position.y + platform.height) {
+  if (player.position.x + player.width >= platforms.position.x
+    && player.position.x <= platforms.position.x + platforms.width
+    && player.position.y + player.height >= platforms.position.y
+    && player.position.y <= platforms.position.y + platforms.height) {
     player.velocity.x = 0
+    // player.velocity.y = 2
   }
 }
 animate()
 
-// ! Arrow keys for movement 
+
+// ! Event Listeners
 addEventListener('keydown', ({ keyCode }) => {
   switch (keyCode) {
     case 37:
       keys.left.pressed = true
-      console.log('left')
+      lastKey = 37
       break;
-
     case 39:
       keys.right.pressed = true
-      console.log('right')
+      lastKey = 39
       break;
-
     case 38:
-      player.velocity.y -= 10
-      console.log('up')
+      if (player.velocity.y === 0) {
+        player.velocity.y -= 5
+        // console.log('up')
+      }
       break;
   }
 })
@@ -133,13 +142,8 @@ addEventListener('keyup', ({ keyCode }) => {
     case 37:
       keys.left.pressed = false
       break;
-
     case 39:
       keys.right.pressed = false
-      break;
-
-    case 38:
-      player.velocity.y = 0
       break;
   }
 })
